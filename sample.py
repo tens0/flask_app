866196356a18053452673e7bdc16087b6d2df935
tens <douganeko77@gmail.com>
2023-05-20 19:42:15 +0900

    2st commit

---
+++ sample.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+# Flaskをインスタンス化
+app = Flask(__name__)
+
+#ルートディレクトリにアクセスがあった時の処理
+@app.route('/')
+def hello():
+    return 'Hello World!'
+
+#エントリーポイント
+if __name__ == '__main__':
+    app.run()
+    
+ 
